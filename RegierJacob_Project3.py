2644c764cb79b72cfad743ef1b33e8f020532a4d
Jacob <jregier8@uwo.ca>
2024-11-24 15:17:18 -0500

    Implemented the system of dimensionless ODEs for density and mass as functions of radius. Introduced a stability check to avoid invalid computations when r=0.

--- RegierJacob_Project3.py
+++ RegierJacob_Project3.py
@@ -1,4 +1,3 @@
-#commit 1
 import numpy as np
 from scipy.integrate import solve_ivp
 from matplotlib import pyplot as plt
@@ -7,3 +6,13 @@ Mu_e = 2
 RHO_0 = 9.74 * 10**5 * Mu_e
 M_0 = 5.67 * 10**33 / Mu_e**2
 R_0 = 7.72 * 10**8 / Mu_e
+
+
+def system_of_equations(r, ystate):
+    rho, m = ystate
+    if rho <= 0:  
+        return [0, 0]
+    x = rho**(1/3)
+    gamma_x = x**2 / (3 * (1 + x**2)**0.5)
+    dystatedr = [-m * rho / (gamma_x * r**2), rho * r**2]
+    return dystatedr
